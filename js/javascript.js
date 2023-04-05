6c79ce0b093e497057fb5a74ac89d3a0cc1de4d5
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-05 17:15:25 +0200

    add: price

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,16 @@
-const  userKm=prompt("quanti kilometri vuole percorrere?")
+const numberKm = parseInt(prompt('Salve, Quanti km deve percorrere?'));
+console.log(numberKm);
 
-const  ageUser=prompt("età del passeggero")
+const ageUse = parseInt(prompt("Perfetto! Mi dica la sua età"));
+console.log(ageUse);
+
+const price =(0.21);
+console.log(price * numberKm);
+
+if(ageUser < 18) {
+    console.log(price * numberKm * 20 / 100);
+}
+else(ageUse > 18) {
+    console.log(price * numberKm );
+}
+
